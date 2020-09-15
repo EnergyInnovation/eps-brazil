cb514921c186c9fd244a73de572b7cbc240ec2ab
Megan Mahajan <megan@energyinnovation.org>
2020-09-15 10:29:37 -0700

    Calibration script edits

--- GeneratedDataLoggingScript.cmd
+++ GeneratedDataLoggingScript.cmd
@@ -1,9 +1,9 @@
 SPECIAL>LOADMODEL|"EPS.mdl"
 
-SIMULATE>SAVELIST|OutputVarsToExport-Calibration.lst
+SIMULATE>SAVELIST|OutputVarsToExport-Emissions.lst
 SIMULATE>RUNNAME|NoSettings
 SIMULATE>READCIN|
 MENU>RUN|O
-MENU>VDF2TAB|NoSettings.vdf|NoSettings.tsv|OutputVarsToExport-Calibration.lst|||||:NoSettings
+MENU>VDF2TAB|NoSettings.vdf|NoSettings.tsv|OutputVarsToExport-Emissions.lst|||||:NoSettings
 
 SIMULATE>SAVELIST|
